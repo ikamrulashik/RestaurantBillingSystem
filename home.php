15a67a42f8c0ff717f42ecf83823d6efd5db2409
Kamrul Ashik <mdkamrul.islam@northsouth.edu>
2020-12-26 20:44:03 +0600

    athentication part uploaded

---
+++ home.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Home</title>
+    </head>
+    <body>
+        <p>Welcome to restaurant billing system</p>
+    </body>
+    <a href="logout.php">logout</a>
+</html>
